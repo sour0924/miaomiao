6b07bdd5192c7804c4796355f0b3595e0800dd42
sour0924 <15225423579@163.com>
2019-09-19 09:30:26 +0800

    add createComponents

---
+++ src/routers/index.js
@@ -0,0 +1,23 @@
+/**主路由 */
+import Vue from 'vue'
+import Router from 'vue-router'
+import movieRouter from './movie'
+import cinemaRouter from './cinema'
+import mineRouter from './mine'
+
+Vue.use(Router)
+
+export default new Router({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes: [
+    movieRouter,
+    cinemaRouter,
+    mineRouter,
+    //重定向
+    {
+        path : '/*',
+        redirect : '/movie'
+    }
+  ]
+})
---
+++ src/views/Mine/index.vue
@@ -0,0 +1,32 @@
+<!-- 我的喵喵组件页面 -->
+<template>
+    <div id="main">
+        <Header title="我的喵喵"/>
+        <div class="login_body">
+            <Login />
+        </div>
+        <TabBar />
+    </div>
+</template>
+
+<script>
+//这里可以导入其他文件（比如：组件，工具js，第三方插件js，json文件，图片文件等等）
+//例如：import 《组件名称》 from '《组件路径》';
+
+import Header from '@/components/Header';
+import TabBar from '@/components/TabBar';
+import Login from '@/components/Login';
+
+export default {
+    name : 'Mine',
+    components : {
+        Header,
+        TabBar,
+        Login
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
