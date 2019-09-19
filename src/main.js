08eb44cf12c86cc1102e066b444f7f3bdf2cb15f
sour0924 <15225423579@163.com>
2019-09-19 13:53:02 +0800

    add setData

--- src/main.js
+++ src/main.js
@@ -3,6 +3,14 @@ import App from './App.vue'
 import router from './routers'
 import store from './stores'
 
+import axios from 'axios'
+Vue.prototype.axios = axios;//引入axios
+
+//全局过滤图片数据
+Vue.filter('setWH',(url,arg)=>{
+  return url.replace(/w\.h/,arg);
+});
+
 Vue.config.productionTip = false
 
 new Vue({
